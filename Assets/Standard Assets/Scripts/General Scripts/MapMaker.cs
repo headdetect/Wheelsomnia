0ab9de4640e578f67f1cad3dbee8cf0af08102d4
Eddie Penta <ecp4224@gmail.com>
2014-12-20 00:39:28 -0500

    Made script that reads tiled map

---
+++ Assets/Standard Assets/Scripts/General Scripts/MapMaker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapMaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var testFile = Resources.Load<TextAsset> ("test");
+
+		var mapInfo = SimpleJSON.JSON.Parse (testFile.text);
+		foreach (SimpleJSON.JSONNode t in mapInfo["layers"].AsArray) {
+			foreach (SimpleJSON.JSONNode o in t["objects"].AsArray) {
+				float x = o["x"].AsFloat;
+				float y = o["y"].AsFloat;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
