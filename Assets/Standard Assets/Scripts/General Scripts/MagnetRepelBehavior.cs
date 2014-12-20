b637b877170a2606f7b7bfa07157bdfd9b3ac869
sean-beckstead <sean.beckstead@gmail.com>
2014-12-20 07:01:08 -0700

    Finished up with magnet push and pull

---
+++ Assets/Standard Assets/Scripts/General Scripts/MagnetRepelBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetRepelBehavior : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D otherObj)
+	{
+		if (otherObj.tag == "Player") {
+			float distance = Mathf.Abs(Mathf.Sqrt (Mathf.Pow ((transform.position.x - otherObj.transform.position.x),2f) + Mathf.Pow ((transform.position.y - otherObj.transform.position.y),2f)));
+			Vector2 forceDirection = transform.position - otherObj.transform.position ;
+			otherObj.rigidbody2D.AddForce(forceDirection.normalized * ((distance - 13)/18), ForceMode2D.Impulse);
+		}
+	}
+}
