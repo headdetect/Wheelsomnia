eacb8e8fba881a1bdc5c4600675aecb7ad20a25b
headdetect <headdetective2@gmail.com>
2014-12-20 07:44:05 -0700

    Did so much, I cant even put in one commit message.

---
+++ Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour {
+	
+	// The target we are following
+	public Transform target;
+	// The distance in the x-z plane to the target
+	public float distance = 10.0f;
+	// the height we want the camera to be above the target
+	public float height = 5.0f;
+	public float heightDamping = 2.0f;
+	public float rotationDamping = 0.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+		// Early out if we don't have a target
+		if (!target)
+			return;
+
+		var wantedHeight = target.position.y + height;
+		var currentHeight = transform.position.y;
+
+		// Damp the height
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		transform.position = target.position;
+		
+		// Set the height of the camera
+		transform.position = new Vector3(target.position.x, currentHeight, -10f);
+
+		
+		// Always look at the target
+		transform.LookAt (target);
+
+		transform.rotation = new Quaternion (0, 0, 0, 0);
+	}
+}
--- Assets/Standard Assets/Scripts/General Scripts/MapMaker.cs
+++ Assets/Standard Assets/Scripts/General Scripts/MapMaker.cs
@@ -1,67 +1,130 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum GameObjectTypes {
+	Nothing = 0,
+	Wall = 1,
+	Ball = 2,
+	Lava = 3,
+	Flag = 4
+}
+
 public class MapMaker : MonoBehaviour {
 	
 	public GameObject flag;
 	public GameObject ball;
 	public GameObject wall;
 
-	private GameObject PlayerBall { get; set; }
-	private Rigidbody PlayerRigidBody {
-		get {
-			return PlayerBall.rigidbody;
-		}
-	}
+	private GameObject _PlayerBall;
+	private Rigidbody2D _PlayerRigidBody;
+
+	private int cameraSize = 70;
 	
 	// Use this for initialization
 	void Start () {
 		var testFile = Resources.Load<TextAsset> ("test");
 		
 		var mapInfo = SimpleJSON.JSON.Parse (testFile.text);
-		
-		float tileXSize = mapInfo ["tilewidth"].AsFloat;
-		float tileYSize = mapInfo ["tileheight"].AsFloat;
-		
-		float yadd = mapInfo["height"].AsFloat * tileYSize;
-		
+
+		int x = 0;
+		int y = 0;
+
+		int objectScale = 1;
+		int objectScaleLocal = 20;
+
 		foreach (SimpleJSON.JSONNode t in mapInfo["layers"].AsArray) {
-			foreach (SimpleJSON.JSONNode o in t["objects"].AsArray) {
-				string type = o["type"].Value;
-				
-				float x = o["x"].AsFloat;
-				float y = -o["y"].AsFloat + yadd;
-				float width = o["width"].AsFloat;
-				float height = o["height"].AsFloat;
-				
-				x += (width / 2f);
-				y -= (height / 2f);
-				
-				width /= tileXSize;
-				height /= tileYSize;
-				if (type == "Flag") {
-					GameObject obj = Instantiate(flag, new Vector3(x / tileXSize, y / tileYSize, 0), Quaternion.identity) as GameObject;
-					obj.transform.localScale = new Vector3(width, height, 1);
-					obj.transform.position = new Vector3(x / tileXSize, y / tileYSize, 0);
-				} else if (type == "Ball Start") {
-					GameObject obj = Instantiate(ball, new Vector3(x / tileXSize, y / tileYSize, 0), Quaternion.identity) as GameObject;
-					obj.transform.localScale = new Vector3(width, height, 1);
-					obj.transform.position = new Vector3(x / tileXSize, y / tileYSize, 0);
-					PlayerBall = obj;
-				} else {
-					GameObject obj = Instantiate(wall, new Vector3(x / tileXSize, y / tileYSize, 0), Quaternion.identity) as GameObject;
-					obj.transform.localScale = new Vector3(width / 2f, height / 2f, 1);
-					obj.transform.position = new Vector3(x / tileXSize, y / tileYSize, 0);
+			SimpleJSON.JSONArray data = t["data"].AsArray;
+			int height = t["height"].AsInt;
+			int width = t["width"].AsInt;
+
+			cameraSize = Mathf.Max(cameraSize, objectScaleLocal * width);
+
+			for(int h = 0; h < height; h++) {
+				for(int w = 0; w < width; w++) {
+					int index = w * width + h;
+					GameObjectTypes type = (GameObjectTypes)data[index].AsInt;
+					GameObject obj = null;
+					switch(type) {
+						case GameObjectTypes.Ball:
+							obj = Instantiate(ball, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+							obj.transform.localScale = new Vector3(1, 1, 1);
+							obj.transform.position = new Vector3(x, y, 0);
+							_PlayerBall = obj;
+							_PlayerRigidBody = _PlayerBall.rigidbody2D;
+							break;
+						case GameObjectTypes.Flag:
+							obj = Instantiate(flag, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+							obj.transform.localScale = new Vector3(1, 1, 1);
+							obj.transform.position = new Vector3(x, y, 0);
+							break;
+						case GameObjectTypes.Wall:
+							obj = Instantiate(wall, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+							obj.transform.localScale = new Vector3(1, 1, 1);
+							obj.transform.position = new Vector3(x, y, 0);
+							break;
+					}
+
+					x += objectScale * objectScaleLocal / width;
 				}
-				
+				y += objectScale * objectScaleLocal / height;
 			}
 		}
 	}
-	
+
+	public float speed = 150f;
+	public float maxSpeed = 1500f;
+	public float currentAngularVelocity = 0;
+	public float multiplyer = 10f;
+
+	private bool firstPass = false;
+	private bool justLetGoOfSpace = false;
+
+
+	private bool isZoomedOut = false;
+
+	private SmoothFollow followScript = null;
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			PlayerRigidBody.angularVelocity += new Vector3(1, 1, 0);
+		// Attach camera to ball //
+		if (followScript == null)
+			followScript = Camera.main.GetComponent<SmoothFollow> ();
+		followScript.target = _PlayerBall.transform;
+		if (Input.GetKey (KeyCode.Space) && !justLetGoOfSpace) {
+			if(!firstPass) {
+				Debug.Log ("Just pressed space");
+				firstPass = true;
+			}
+			_PlayerRigidBody.angularVelocity = Mathf.Max (_PlayerRigidBody.angularVelocity - speed, -maxSpeed);
+			currentAngularVelocity = _PlayerRigidBody.angularVelocity;
+		} else {
+			if(!justLetGoOfSpace && firstPass) {
+				justLetGoOfSpace = true;
+				Debug.Log ("Just un-pressed space");
+
+				_prepareMaterial();
+				_PlayerRigidBody.collider2D.sharedMaterial = Resources.Load<PhysicsMaterial2D>("Wheel");
+				_PlayerRigidBody.AddForce(new Vector2(-_PlayerRigidBody.angularVelocity * .99f /* The radius */ * multiplyer, 0));
+				_loadMaterial();
+			}
 		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			isZoomedOut = !isZoomedOut;
+			if (isZoomedOut) {
+				camera.orthographicSize = 30f;
+			} else {
+				camera.orthographicSize = 70f;
+			}
+		}
+	}
+
+	// Bug in unity...
+	private void _loadMaterial() {
+		_PlayerRigidBody.collider2D.enabled = true;
+	}
+
+	private void _prepareMaterial() {
+		_PlayerRigidBody.collider2D.enabled = false;
 	}
 }
