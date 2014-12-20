943eac4af4f917bd676b510e0597253a1f6b50ee
Eddie Penta <ecp4224@gmail.com>
2014-12-20 01:14:13 -0500

    MapMaker now places cubes

--- Assets/Standard Assets/Scripts/General Scripts/MapMaker.cs
+++ Assets/Standard Assets/Scripts/General Scripts/MapMaker.cs
@@ -8,10 +8,23 @@ public class MapMaker : MonoBehaviour {
 		var testFile = Resources.Load<TextAsset> ("test");
 
 		var mapInfo = SimpleJSON.JSON.Parse (testFile.text);
+		float yadd = mapInfo["height"].AsFloat * mapInfo["tileheight"].AsFloat;
 		foreach (SimpleJSON.JSONNode t in mapInfo["layers"].AsArray) {
 			foreach (SimpleJSON.JSONNode o in t["objects"].AsArray) {
 				float x = o["x"].AsFloat;
-				float y = o["y"].AsFloat;
+				float y = -o["y"].AsFloat + yadd;
+				float width = o["width"].AsFloat;
+				float height = o["height"].AsFloat;
+
+				x += (width / 2f);
+				y -= (height / 2f);
+
+				width /= 16f;
+				height /= 16f;
+
+				GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				obj.transform.localScale = new Vector3(width, height, 1);
+				obj.transform.position = new Vector3(x / 10f, y / 10f, 0);
 			}
 		}
 	}
