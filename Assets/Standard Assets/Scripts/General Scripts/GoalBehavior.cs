72b02c3c35d9f8c6d92af2f374afbfae519db788
headdetect <headdetective2@gmail.com>
2014-12-31 15:55:56 -0700

    Adding tools and such

--- Assets/Standard Assets/Scripts/General Scripts/GoalBehavior.cs
+++ Assets/Standard Assets/Scripts/General Scripts/GoalBehavior.cs
@@ -1,28 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoalBehavior : MonoBehaviour {
-
-	Rect windowRect = new Rect(20,20,120,50);
+public class GoalBehavior : MonoBehaviour
+{
+	Rect windowRect = new Rect (20, 20, 120, 50);
 	bool found = false;
 
-	void OnTriggerEnter2D(Collider2D otherObj)
+	void OnTriggerEnter2D (Collider2D otherObj)
 	{
 		if (otherObj.tag == "Player") {
 			found = true;
 		}
 	}
 
-	void OnGUI() {
+	void OnGUI ()
+	{
 		if (found)
 			windowRect = GUI.Window (0, windowRect, WinningFunction, "You won!");
 	}
 
-	void WinningFunction(int windowID) {
-		if (GUI.Button (new Rect(10,20,100,20), "You won!"))
-		{
-		    print("yes");
-		    Application.LoadLevel(1);
+	void WinningFunction (int windowID)
+	{
+		if (GUI.Button (new Rect (10, 20, 100, 20), "You won!")) {
+			print ("yes");
+			Application.LoadLevel (1);
 		}
 	}
 }
---
+++ Assets/Standard Assets/Scripts/General Scripts/Tool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tool : MonoBehaviour
+{
+    Image toolImage;
+    Text toolCount;
+
+    // Use this for initialization
+    void Start()
+    {
+        toolImage = GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        toolImage.color -= Color.white * Time.deltaTime;
+    }
+}
